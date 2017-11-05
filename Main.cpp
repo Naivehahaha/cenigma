73168ddf20419fa801aaf7255031831b2e081025
sqy12 <victor.yan002@gmail.com>
2017-11-05 22:17:26 +0000

    Simplicification

--- Main.cpp
+++ Main.cpp
@@ -8,89 +8,26 @@ using namespace std;
 
 const int Num_Alphabet = 26;
 
-int mappingA(char alph) {
-	return alph - 'A';
-}
-
-char mappingI(int number) {
-	return 'A' + number;
-}
-char reflector(char alph) {
-	return mappingI((mappingA(alph) + 13) % Num_Alphabet);
-}
-
-class component {
-protected:
-	component(int arrayIn[Num_Alphabet]);
-	virtual ~component();
-	int array[Num_Alphabet];
-//	int arrayInput[Num_Alphabet];
-	int * arrayInput;
-public:
-	int getposition(int position) {
-		return array[position];
-	}
-
-	int findposition(int number) {
-		int i;
-		for (i = 0; i < Num_Alphabet; i++) {
-			if (array[i] == number) {
-				break;
-			}
-		}
-		return i;
-	}
-
-	void initial() {
-		int i;
-		for (i = 0; i < Num_Alphabet; i++) {
-			array[i] = i;
-		}
-	}
-	virtual void process();
-};
-
-class rotor: public component {
-public:
-	rotor(int arrayIn[Num_Alphabet]);
-	virtual ~rotor();
-	virtual void process();
-};
-
-class plugboard: public component {
+class plugboard{
 public:
 	plugboard(int arrayIn[Num_Alphabet]);
 	virtual ~plugboard();
-	virtual void process();
+	void process();
+private:
+	int array[Num_Alphabet];
+	int arrayInput[Num_Alphabet];
 };
 
 plugboard::~plugboard() {
 }
 
-rotor::~rotor() {
-}
-
-component::component(int arrayIn[Num_Alphabet]) {
-	arrayInput = arrayIn;
-	initial();
-//	array<int, 26>
-}
-
-void component::process() {
-}
-component::~component() {
-}
-
-rotor::rotor(int arrayIn[Num_Alphabet]) :
-		component(arrayIn) {
-}
-
 plugboard::plugboard(int arrayIn[Num_Alphabet]) :
-		component(arrayIn) {
-}
-
-void rotor::process() {
-
+{
+	arrayInput = arrayIn;
+	int i;
+	for (i = 0; i < Num_Alphabet; i++) {
+		array[i] = i;
+	}
 }
 
 void plugboard::process() {
@@ -132,31 +69,21 @@ string readinput() {
 	return input;
 }
 
-string encode(char input, int numofrotor, rotor* rotorlist, plugboard* plug) {
-	(*plug).process();
-
-	return "das";
-}
-
 int main(int argc, char **argv) {
 	int numberoffile = argc - 1;
-//	ifstream file[numberoffile];
 	basic_ifstream<char>* file;
-	int i;
-	int j;
-	rotor* rotorlist[numberoffile - 1];
-	for (i = 0; i < numberoffile - 1; i++) {
+
+	for (int i = 0; i < numberoffile - 1; i++) {
 		file[i].open(argv[i + 1]);
-		string line;
 		int arrayIn[26];
-		for (j = 0; j < Num_Alphabet; j++) {
+		for (int j = 0; j < Num_Alphabet; j++) {
 			file[i] >> arrayIn[j];
 		}
-		rotorlist[i] = new rotor(arrayIn);
 		file[i].close();
 	}
+	
 	int arrayIn[26];
-	for (i = 0; i < Num_Alphabet; i++) {
+	for (int i = 0; i < Num_Alphabet; i++) {
 		file[numberoffile - 1].open(argv[numberoffile]);
 		j = 0;
 		int tmp;
@@ -168,11 +95,15 @@ int main(int argc, char **argv) {
 			arrayIn[j] = 50;
 		}
 	}
-	plugboard plug(arrayIn);
-	string input = readinput();
-	for (i = 0; i < input.length(); i++) {
-		encode(input[i], numberoffile - 1, rotorlist, plug);
+	
+	// No idea what this part does
+	{
+		plugboard plug(arrayIn);
+		string input = readinput();
+		for (i = 0; i < input.length(); i++) {
+			plug.process();
+		}
 	}
+	
 	return 0;
-
 }
